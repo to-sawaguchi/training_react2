53b61b1b7e44ab46121c61b57d2195aa71fa35b6
to-sawaguchi <123784874+to-sawaguchi@users.noreply.github.com>
2023-06-04 20:41:01 +0900

    Add files via upload

---
+++ precache-manifest.b4b329fdaab8a2322505859895786f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9259d234ae2b94d98bd9e52979fa4e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a3680dd688e34ae8412",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8380ba43b34f367fc359",
+    "url": "/static/js/2.10a97f2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.10a97f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a3680dd688e34ae8412",
+    "url": "/static/js/main.752b6a28.chunk.js"
+  },
+  {
+    "revision": "9030c7668e656f2f612a",
+    "url": "/static/js/runtime-main.6c5be910.js"
+  }
+]);
