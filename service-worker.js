97264b0f1fa5c0824999bdf1ba7be2467b20982e
to-sawaguchi <123784874+to-sawaguchi@users.noreply.github.com>
2023-06-04 21:36:19 +0900

    Add files via upload
    version9に戻す

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4b329fdaab8a2322505859895786f3b.js"
+  "/precache-manifest.95d4f13d832ff778384a6649949be4fe.js"
 );
 
 self.addEventListener('message', (event) => {
